adf5cddb95c0cb288106319b1e2e58bde9b4f7e6
Randall Delafuente <randalld.ftc@gmail.com>
2019-05-04 20:37:32 -0400

    Cleaned up repo
    read summary

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/Mecanum.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.subsytems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.List;
+
+public class Mecanum {
+
+    private List<DcMotor> motors;
+    private List<DcMotor> encoders;
+}
